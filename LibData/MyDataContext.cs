9acffd10d8e790171381f1b5ca4b14282a82abe9
Sergiy <zaporojechs@gmail.com>
2022-10-15 22:01:22 +0300

    first

---
+++ LibData/MyDataContext.cs
@@ -0,0 +1,31 @@
+﻿using LibData.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibData
+{
+    public class MyDataContext : DbContext
+    {
+        public MyDataContext()
+        {
+            this.Database.Migrate();
+        }
+        public DbSet<UserEntitie> Users { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("" +
+                "Server=localhost;" +
+                "Port=5432;" +
+                "Database=wpfdata;" +
+                "User Id=postgres;" +
+                "Password=123456;");
+        }
+    }
+}
---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using LibData;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void mFileExit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void mActionREgister_Click(object sender, RoutedEventArgs e)
+        {
+            RegistrWindow window = new RegistrWindow();
+            window.ShowDialog();
+        }
+
+ 
+        private void mActionLogin_Click(object sender, RoutedEventArgs e)
+        {
+            LoginWindow window = new LoginWindow();
+            window.ShowDialog();
+        }
+
+        private void btnConnetion_Click(object sender, RoutedEventArgs e)
+        {
+            using (MyDataContext context = new MyDataContext())
+            {
+                Stopwatch stopWatch = new Stopwatch();
+                stopWatch.Start();
+
+                btnConnetion.Content += " - " + context.Users.Count().ToString();
+                //MessageBox.Show(context.Users.Count().ToString());
+
+                // Get the elapsed time as a TimeSpan value.
+                TimeSpan ts = stopWatch.Elapsed;
+
+                // Format and display the TimeSpan value.
+                string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                  ts.Hours, ts.Minutes, ts.Seconds,
+                  ts.Milliseconds / 10);
+                MessageBox.Show("RunTime " + elapsedTime);
+            }
+        }
+    }
+}
