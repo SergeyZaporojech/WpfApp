04b405196b18362caf314d8cb66f2bf3b989b172
Sergiy <zaporojechs@gmail.com>
2022-10-16 15:32:09 +0300

    "Thread_1"

---
+++ _1_Thread/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace _1_Thread
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var idThread = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine("Main thread: {0}", idThread);
+            Thread queue = new Thread(Queue);
+            queue.Start();
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            //Queue();
+            SendMessendg();
+            queue.Join();  //чекає завершення потоку який додатуово запущений
+
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+            // Format and display the TimeSpan value.
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime " + elapsedTime);
+
+        }
+
+        private static void SendMessendg()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(300);
+                Console.WriteLine("Send messendg {0}", i + 1);
+            }
+        }
+
+
+        private static void Queue()
+        {
+            var idThread = Thread.CurrentThread.ManagedThreadId;
+            var comsoleColor = Console.ForegroundColor;
+            Console.WriteLine("Other thread: {0}", idThread);
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(200);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Обробка пасажира {0}", i + 1);
+                Console.ForegroundColor = comsoleColor;
+            }
+        }
+
+
+    }
+}
---
+++ _1_Thread_/Program.cs
@@ -0,0 +1,95 @@
+﻿using LibData;
+using System.Diagnostics;
+
+namespace _1_Thread
+{
+    public delegate void ConnectionCompleteDelegate(MyDataContext context);
+    internal class Program
+    {
+
+        public static event ConnectionCompleteDelegate _connectionComplete;
+        public static void Main(string[] args)
+        {
+
+            _connectionComplete += Program__onnectionComplete;
+            var idThread = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine("Main thread: {0}", idThread);
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+
+            //Thread queue = new Thread(Queue);
+            //queue.Start();
+            ////Queue();
+            //SendMessendg();
+            //queue.Join();  //чекає завершення потоку який додатуово запущений
+
+            Thread connect = new Thread(ConnectionDatabase);
+            connect.Start();
+            //var countUsers = myDataContext.Users.Count();
+            
+
+            stopWatch.Stop();
+            // Get the elapsed time as a TimeSpan value.
+            TimeSpan ts = stopWatch.Elapsed;
+
+        // Format and display the TimeSpan value.
+        string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+        Console.WriteLine("RunTime " + elapsedTime);
+
+        }
+
+        private static void Program__onnectionComplete(MyDataContext context)
+        {
+            Console.WriteLine("Even completed connection {0}", Thread.CurrentThread.ManagedThreadId);
+        }
+
+        private static void ConnectionDatabase()
+        {           
+            ShowMessenge("Begin connection database", ConsoleColor.Red) ;
+            MyDataContext myDataContext = new MyDataContext();
+            ShowMessenge("Connection database complited",ConsoleColor.Red) ;
+            if (_connectionComplete!= null)
+            {
+                _connectionComplete(myDataContext);
+            }
+        }
+        private static void ShowMessenge(string text, ConsoleColor color)
+        {
+            var comsoleColor = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ForegroundColor = comsoleColor;
+        }
+
+
+
+
+        private static void SendMessendg()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(300);
+                Console.WriteLine("Send messendg {0}", i + 1);
+            }
+        }
+
+
+        private static void Queue()
+        {
+            var idThread = Thread.CurrentThread.ManagedThreadId;
+            var comsoleColor = Console.ForegroundColor;
+            Console.WriteLine("Other thread: {0}", idThread);
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(200);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Обробка пасажира {0}", i + 1);
+                Console.ForegroundColor = comsoleColor;
+            }
+        }
+
+
+    }
+}
